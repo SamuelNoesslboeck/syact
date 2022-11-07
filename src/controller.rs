7248b92619d26f6bf523e13e56e96539c336569c
Samuel Nösslböck <114945809+SamuelNoesslboeck@users.noreply.github.com>
2022-11-07 14:22:36 +0100

    Added constructions

--- src/controller.rs
+++ src/controller.rs
@@ -1,5 +1,5 @@
 use std::{thread, time, vec};
-use std::f64::consts::PI;
+use std::f32::consts::PI;
 
 use gpio::{sysfs::*, GpioOut};
 
@@ -19,24 +19,26 @@ pub trait StepperCtrl
     fn get_data(&self) -> &StepperData;
 
     /// Move a single step
-    fn step(&mut self, time : f64);
+    fn step(&mut self, time : f32);
 
     /// Accelerates the motor as fast as possible to the given speed, canceling if it takes any longer than the stepcount given. The function returns the steps needed for the acceleration process
-    fn accelerate(&mut self, stepcount : u64, omega : f64) -> Vec<f64>;
+    fn accelerate(&mut self, stepcount : u64, omega : f32) -> Vec<f32>;
     /// Drive a curve of step times, represented by a list
-    fn drive_curve(&mut self, curve : &Vec<f64>);
+    fn drive_curve(&mut self, curve : &Vec<f32>);
     /// Move a number of steps as fast as possible, the steps will be traveled without 
-    fn steps(&mut self, stepcount : u64, omega : f64);
+    fn steps(&mut self, stepcount : u64, omega : f32);
+    /// Drive a certain distance, returns the actual distance traveled
+    fn drive(&mut self, distance : f32, omega : f32) -> f32;
     /// Move a number of steps safefy (with loads included)
-        // fn steps_save(&mut self, stepcount : u64, omega : f64, up_load : UpdateLoadFunc);
-        // fn steps_update(&mut self, stepcount : u64, omega : f64, up_load : UpdateLoadFunc, up_pos : UpdatePosFunc);
+        // fn steps_save(&mut self, stepcount : u64, omega : f32, up_load : UpdateLoadFunc);
+        // fn steps_update(&mut self, stepcount : u64, omega : f32, up_load : UpdateLoadFunc, up_pos : UpdatePosFunc);
     // Stops the motor as fast as possible
     fn stop(&mut self) -> u64;
 
     /// Get the current speed
-    fn get_speed(&self) -> f64;
+    fn get_speed(&self) -> f32;
     /// Sets the speed after accelerating
-    fn set_speed(&mut self, omega : f64);
+    fn set_speed(&mut self, omega : f32);
 
     /// Get the current direction of the motor
     /// (True for right, False for left)
@@ -46,17 +48,17 @@ pub trait StepperCtrl
     fn set_dir(&mut self, dir : bool);
 
     /// Returns the absolute position in radians
-    fn get_abs_pos(&self) -> f64;    
+    fn get_abs_pos(&self) -> f32;    
     /// Returns the relative position (current rotation) in radians
-    fn get_rel_pos(&self) -> f64;
+    fn get_rel_pos(&self) -> f32;
 
     /// Moves the motor in the current direction till the measure pin is true
-    fn measure(&mut self, max_steps : u64, omega : f64) -> Option<()>;
+    fn measure(&mut self, max_steps : u64, omega : f32) -> Option<()>;
 
     // /// Sets the current absolute position
-    // fn set_absolute_pos(&mut self) -> f64;
+    // fn set_absolute_pos(&mut self) -> f32;
     // /// Sets the current relative position
-    // fn set_relative_pos(&mut self) -> f64;
+    // fn set_relative_pos(&mut self) -> f32;
 }
 
 pub struct PwmStepperCtrl
@@ -64,9 +66,9 @@ pub struct PwmStepperCtrl
     /// Stepper data
     pub data : StepperData,
     /// Safety factor for load and speed calculations
-    pub sf : f64,
+    pub sf : f32,
     /// Curve factor for slower acceleration
-    pub cf : f64, 
+    pub cf : f32, 
 
     /// Pin for controlling direction
     pub pin_dir : u16,
@@ -86,7 +88,7 @@ pub struct PwmStepperCtrl
     sys_step : SysFsGpioOutput,
     // sys_mes : Option<SysFsGpioInput>,
 
-    omega : f64
+    omega : f32
 }
 
 impl PwmStepperCtrl
@@ -117,8 +119,8 @@ impl StepperCtrl for PwmStepperCtrl
         return &self.data;    
     }
 
-    fn step(&mut self, time : f64) {
-        let step_time_half = time::Duration::from_secs_f64(time / 2.0);
+    fn step(&mut self, time : f32) {
+        let step_time_half = time::Duration::from_secs_f32(time / 2.0);
 
         self.sys_step.set_high().unwrap();
         thread::sleep(step_time_half);
@@ -128,11 +130,11 @@ impl StepperCtrl for PwmStepperCtrl
         self.pos += if self.dir { 1 } else { -1 };
     }
 
-    fn accelerate(&mut self, stepcount : u64, omega : f64) -> Vec<f64> {
+    fn accelerate(&mut self, stepcount : u64, omega : f32) -> Vec<f32> {
         let t_start = self.sf / start_frequency(&self.data);
         let t_min = self.data.time_step(omega);
 
-        let mut time_step : f64;      // Time per step
+        let mut time_step : f32;      // Time per step
         let mut i: u64 = 1;                 // Step count
         let mut curve = vec![];
 
@@ -142,7 +144,7 @@ impl StepperCtrl for PwmStepperCtrl
                 break;
             }
 
-            time_step = t_start / (i as f64).powf(0.5 * self.cf);
+            time_step = t_start / (i as f32).powf(0.5 * self.cf);
 
             if time_step < t_min {
                 self.set_speed(omega);
@@ -157,13 +159,13 @@ impl StepperCtrl for PwmStepperCtrl
         return curve;
     }
 
-    fn drive_curve(&mut self, curve : &Vec<f64>) {
+    fn drive_curve(&mut self, curve : &Vec<f32>) {
         for i in 0 .. curve.len() {
             self.step(curve[i]);
         }
     }
 
-    fn steps(&mut self, stepcount : u64, omega : f64) {
+    fn steps(&mut self, stepcount : u64, omega : f32) {
         let curve = self.accelerate(stepcount / 2, omega);
         let time_step = self.data.time_step(omega);
         let last = curve.last().unwrap_or(&time_step);
@@ -185,16 +187,30 @@ impl StepperCtrl for PwmStepperCtrl
         self.drive_curve(&curve);
         self.set_speed(0.0);
     }
+
+    fn drive(&mut self, distance : f32, omega : f32) -> f32 {
+        if distance == 0.0 {
+            return 0.0;
+        } else if distance > 0.0 {
+            self.set_dir(true);
+        } else if distance < 0.0 {
+            self.set_dir(true);
+        }
+
+        let steps : u64 = (distance.abs() / self.data.ang_dis()).round() as u64;
+        self.steps(steps, omega);
+        return steps as f32 * self.data.ang_dis();
+    }
     
     fn stop(&mut self) -> u64 {
         0 // TODO
     }
 
-    fn get_speed(&self) -> f64 {
+    fn get_speed(&self) -> f32 {
         return self.omega;
     }
 
-    fn set_speed(&mut self, omega : f64) {
+    fn set_speed(&mut self, omega : f32) {
         self.omega = omega;
     }
 
@@ -208,15 +224,15 @@ impl StepperCtrl for PwmStepperCtrl
         self.sys_dir.set_value(dir).unwrap();
     }
 
-    fn get_abs_pos(&self) -> f64 {
-        return 2.0 * PI * self.pos as f64 / self.data.n_s as f64;
+    fn get_abs_pos(&self) -> f32 {
+        return 2.0 * PI * self.pos as f32 / self.data.n_s as f32;
     }
 
-    fn get_rel_pos(&self) -> f64 {
-        return 2.0 * PI * (self.pos % self.data.n_s as i64) as f64 / self.data.n_s as f64;
+    fn get_rel_pos(&self) -> f32 {
+        return 2.0 * PI * (self.pos % self.data.n_s as i64) as f32 / self.data.n_s as f32;
     }
 
-    fn measure(&mut self, max_steps : u64, omega : f64) -> Option<()> {
+    fn measure(&mut self, max_steps : u64, omega : f32) -> Option<()> {
         let mut curve = self.accelerate(max_steps / 2, omega);
         
         curve.reverse();
@@ -232,18 +248,18 @@ pub struct Cylinder
     pub ctrl : Box<dyn StepperCtrl>,
 
     /// Distance traveled per rad [in mm]
-    pub rte_ratio : f64,
+    pub rte_ratio : f32,
     
     /// Minimal extent [in mm]
-    pub pos_min : f64,
+    pub pos_min : f32,
     /// Maximal extent [in mm]
-    pub pos_max : f64
+    pub pos_max : f32
 }
 
 impl Cylinder
 {
     /// Create a new cylinder instance
-    pub fn new(ctrl : Box<dyn StepperCtrl>, rte_ratio : f64, pos_min : f64, pos_max : f64) -> Self {
+    pub fn new(ctrl : Box<dyn StepperCtrl>, rte_ratio : f32, pos_min : f32, pos_max : f32) -> Self {
         return Cylinder {
             ctrl,
             rte_ratio,
@@ -258,34 +274,77 @@ impl Cylinder
     }
 
     // Conversions
-        pub fn phi_c(&self, dis_c : f64) -> f64 {
+        pub fn phi_c(&self, dis_c : f32) -> f32 {
             dis_c / self.rte_ratio
         }
 
-        pub fn dis_c(&self, phi_c : f64) -> f64 {
+        pub fn dis_c(&self, phi_c : f32) -> f32 {
             phi_c * self.rte_ratio
         }
 
-        pub fn omega_c(&self, v_c : f64) -> f64 {
+        pub fn omega_c(&self, v_c : f32) -> f32 {
             v_c / self.rte_ratio
         }
 
-        pub fn v_c(&self, omega : f64) -> f64 {
+        pub fn v_c(&self, omega : f32) -> f32 {
             omega * self.rte_ratio
         }
     //
 
     /// Extend the cylinder by a given distance _dis_ (in mm) with the maximum velocity _v max_ (in mm/s), returns the actual distance traveled
-    pub fn extend(&mut self, dis : f64, v_max : f64) -> f64 {
+    pub fn extend(&mut self, dis : f32, v_max : f32) -> f32 {
         let steps = (self.phi_c(dis) / self.data().ang_dis()).floor() as u64;
 
         self.ctrl.steps(steps, self.omega_c(v_max));
 
-        return steps as f64 * self.data().ang_dis();
+        return steps as f32 * self.data().ang_dis();
     }
 
     /// Returns the extension of the cylinder
-    pub fn get_ext(&self) -> f64 {
+    pub fn length(&self) -> f32 {
         return self.dis_c(self.ctrl.get_abs_pos());
+    }
+}
+
+pub struct CylinderTriangle 
+{
+    // Triangle
+    pub l_a : f32,
+    pub l_b : f32,
+
+    // Cylinder length
+    pub cylinder : Cylinder
+}
+
+impl CylinderTriangle 
+{
+    pub fn length_for_gamma(&self, gam : f32) -> f32 {
+        (self.l_a.powi(2) + self.l_b.powi(2) + 2.0 * self.l_a * self.l_b * gam.cos()).powf(0.5)
+    }
+
+    pub fn gamma(&self) -> f32 {
+        ((self.cylinder.length().powi(2) as f32 + self.l_a.powi(2) + self.l_b.powi(2)) / 2.0 / self.l_a / self.l_b).acos()
+    }
+
+    pub fn set_gamma(&mut self, gam : f32, v_max : f32) {
+        self.cylinder.extend(self.length_for_gamma(gam), v_max);
+    }
+}
+
+pub struct GearBearing 
+{
+    pub ctrl : Box<dyn StepperCtrl>,
+    
+    pub ratio : f32
+}
+
+impl GearBearing 
+{
+    pub fn get_pos(&self) -> f32 {
+        self.ctrl.get_abs_pos() * self.ratio
+    }
+
+    pub fn set_pos(&mut self, pos : f32, omega : f32) -> f32 {
+        self.ctrl.drive((pos - self.get_pos()) / self.ratio, omega)
     }
 }
