cacbab61759d7d1ad6e7f7bce453f3f4c8f0e36f
Samuel Nösslböck <114945809+SamuelNoesslboeck@users.noreply.github.com>
2022-11-13 13:40:58 +0100

    Fixed interpreter

--- src/gcode.rs
+++ src/gcode.rs
@@ -1,24 +1,22 @@
 use std::collections::HashMap;
 
-use gcode::{Mnemonic};
+pub type GCodeFunc<T> = fn (&mut T, &GCode) -> Option<()>;
 
-pub type GCodeFunc<T> = fn (&Interpreter<T>, &GCode, &T) -> Option<()>;
-
-pub type Letter = Mnemonic;
+pub type Letter = gcode::Mnemonic;
 pub type GCode = gcode::GCode;
 
 pub type NumEntries<T> = HashMap<u32, GCodeFunc<T>>;
 pub type LetterEntries<T> = HashMap<Letter, NumEntries<T>>;
 
-pub struct Interpreter<'a, T>
+pub struct Interpreter<T>
 {
     funcs : LetterEntries<T>,
-    pub mach : &'a T
+    pub mach : T
 }
 
-impl<'a, T> Interpreter<'a, T>
+impl<T> Interpreter<T>
 {   
-    pub fn new(mach : &'a T, funcs : LetterEntries<T>) -> Self {
+    pub fn new(mach : T, funcs : LetterEntries<T>) -> Self {
         return Interpreter {
             funcs,
             mach
@@ -29,12 +27,14 @@ impl<'a, T> Interpreter<'a, T>
         println!("{}", line);
     }
 
-    pub fn interpret(&self, gc_str : &str) {
+    pub fn interpret(&mut self, gc_str : &str) {
         let mut line_count : u64 = 0;
 
         for gc_line in gcode::parse(gc_str) {
-            self.get_func(&gc_line).and_then(|func| {
-                func(&self, &gc_line, self.mach)
+            let func_res = get_func(&self.funcs, &gc_line);
+
+            func_res.and_then(|func| {
+                func(&mut self.mach, &gc_line)
             });
 
             line_count += 1;
@@ -42,11 +42,11 @@ impl<'a, T> Interpreter<'a, T>
 
         self.log_ln(format!("Interpreted {} lines", line_count).as_str());
     }
-    
-    /// Get the GCode Function stored for the given code
-    pub fn get_func(&self, gc : &GCode) -> Option<&GCodeFunc<T>> {
-        self.funcs.get(&gc.mnemonic()).and_then(|v| {
-            v.get(&gc.major_number())
-        })
-    }
+}
+
+/// Get the GCode Function stored for the given code
+pub fn get_func<'a, T>(funcs : &'a LetterEntries<T>, gc : &'a GCode) -> Option<&'a GCodeFunc<T>> {
+    funcs.get(&gc.mnemonic()).and_then(|v| {
+        v.get(&gc.major_number())
+    })
 }
