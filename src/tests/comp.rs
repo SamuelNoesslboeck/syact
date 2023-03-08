a10c8e9278201a315175dd4739c810930a9bdfb9
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2023-03-08 15:44:20 +0100

    Upgraded tests

--- src/tests/comp.rs
+++ src/tests/comp.rs
@@ -1,8 +1,7 @@
 use crate::{StepperConst, StepperCtrl, Component};
-use crate::comp::GearBearing;
+use crate::comp::{GearBearing, Cylinder, CylinderTriangle};
 use crate::data::LinkedData;
 use crate::units::*;
-
 
 #[test]
 fn gear_bearing() {
@@ -13,4 +12,26 @@ fn gear_bearing() {
     gear.link(LinkedData::GEN);
 
     gear.drive_rel(Delta(0.5), Omega(0.5));
+}
+
+#[test]
+fn cylinder() {
+    const PIN_DIR : u8 = 22;
+    const PIN_STEP : u8 = 11;
+
+    let mut cylinder = Cylinder::new(StepperCtrl::new(StepperConst::MOT_17HE15_1504S, PIN_DIR, PIN_STEP), 1.27);
+    cylinder.link(LinkedData::GEN);
+
+    cylinder.drive_rel(Delta(25.0), Omega(25.0));
+}
+
+#[test]
+fn cylinder_bearing() {
+    const PIN_DIR : u8 = 22;
+    const PIN_STEP : u8 = 11;
+
+    let mut cyl_tri = CylinderTriangle::new(Cylinder::new(StepperCtrl::new(StepperConst::MOT_17HE15_1504S, PIN_DIR, PIN_STEP), 1.27), 200.0, 200.0);
+    cyl_tri.link(LinkedData::GEN);
+
+    cyl_tri.drive_rel(Delta(0.5), Omega(0.5));
 }
