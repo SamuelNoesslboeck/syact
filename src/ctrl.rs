4c9e1a31e5cbecd2472ae217ece66e5c61ba9674
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2023-11-12 23:17:20 +0100

    sync

--- src/ctrl.rs
+++ src/ctrl.rs
@@ -52,6 +52,7 @@ use atomic_float::AtomicF32;
     /// Stepper motors and controllers in different resolutions
     pub mod stepper;
     pub use stepper::{Stepper, Controller, GenericPWM};
+    use sylo::Direction;
 //
 
 // Interruptor
@@ -61,6 +62,8 @@ use atomic_float::AtomicF32;
         /// - If `None` the interruptor is not dependent on a movement direction
         /// - If `Some` the interruptor is only active when moving in the given direction
         fn dir(&self) -> Option<sylo::Direction>;
+
+        fn set_temp_dir(&mut self, dir : Direction);
 
         /// Runs a check of the movement process
         fn check(&mut self, gamma : &Arc<AtomicF32>) -> Option<InterruptReason>;
